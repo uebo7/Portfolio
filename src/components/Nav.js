8deefae36b8bbdd27d1d0c3e20298bd2ae9dd555
Logan <logkauf14@gmail.com>
2023-05-03 14:20:50 -0400

    wip: adds nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,23 @@ import { Link } from 'react-router-dom'
 function Nav(props) {
     return (
         <div>
-            <Link to='#about' >
-            
-            </Link>
+            <ul>
+                <li>
+                    <Link to='#about' >
+                        <button>About Me</button>
+                    </Link> 
+                </li>
+                <li> 
+                    <Link to='#projects'>
+                        <button>My Projects</button>
+                    </Link> 
+                </li>
+                <li> 
+                    <Link to='#contact'>
+                        <button>Contact Me Here</button>
+                    </Link> 
+                </li>
+            </ul>
         </div>
     )
 }
