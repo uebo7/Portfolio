fd1648b01a84dd7bc0cef149ff8fd201a8eec5bd
Logan <logkauf14@gmail.com>
2023-05-13 14:42:25 -0400

    wip: adds clickable buttons to skills page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ function Contact(props) {
     return (
         <div className="bg-dark container-fluid">
             <Nav />
-            <h1 className="text-light text-center m-3">Contact Me Here</h1>
+            <h1 className="text-light text-center m-3">Contact Me On LinkedIn</h1>
             <div className="text-center m-5">
                 <a href="https://www.linkedin.com/in/logankaufman/">
                     <img className='linkedin m-5' src={props.img} alt={props.name} />
--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -1,38 +1,65 @@
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import { React, useState } from 'react'
 
 function Skill() {
+
+    const [showCss, setShowCss] = useState(false)
+    const [showSkills, setShowSkills] = useState(false)
+    const [showJavascript, setShowJavascript] = useState(false)
+    const [showPython, setShowPython] = useState(false)
+    
     return (
         <div className='container-fluid text-center bg-dark'>
             <Nav />
             <h1 className='m-5 text-light bg-dark'>Skills</h1>
             <div className="row bg-dark">
                 <div className="col-3">
-                    <button className='btn btn-danger'>HTML</button>
+                    <button className='btn btn-danger' onClick={() => setShowSkills(!showSkills)}>HTML + General skills</button>
                     <div className='div1 text-light'>
-
+                        {showSkills && 
+                            <ul>
+                                <h3>Github</h3>
+                            </ul>
+                        }
                     </div>
                 </div>
                 <div className="col-3">
-                    <button className='btn btn-danger'>CSS</button>
-                    <div className="text-light" id='div2 '>
-                        Bootstrap
+                    <button className='btn btn-danger' onClick={() => setShowCss(!showCss)}>CSS</button>
+                    <div className="text-light" >
+                        {showCss && 
+                            <ul>
+                                <h3>Bootstrap</h3>
+                                <br />
+                                <h3>Materialize</h3>
+                            </ul>
+                        }
                     </div>
                 </div>
                 <div className="col-3">
-                    <button className='btn btn-danger'>Javascript</button>
+                    <button className='btn btn-danger' onClick={() => setShowJavascript(!showJavascript)}>Javascript</button>
                     <div className="div3 text-light">
-                        jQuery
-                        <br />
-                        React.js
-                        <br />
-                        Node.js
+                        {showJavascript &&
+                            <ul>
+                                <h3>jQuery</h3>
+                                <br />
+                                <h3>React.js</h3>
+                                <br />
+                                <h3>Node.js</h3>
+                            </ul>
+                        }
                     </div>
                 </div>
                 <div className="col-3">
-                    <button className='btn btn-danger'>Python</button>
+                    <button className='btn btn-danger' onClick={() => setShowPython(!showPython)}>Python</button>
                     <div className="div4 text-light">
-                        Django
+                        {showPython && 
+                            <ul>
+                                <h3>Django</h3>
+                                <br />
+                                <h3>PostreSQL</h3>
+                            </ul>
+                        }
                     </div>
                 </div>
             </div>
